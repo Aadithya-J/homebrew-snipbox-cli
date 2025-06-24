6b84eb5af970abb30f9b2ddc5503e1c9b8d25cb4
Aadithya-J <jlaadithya@gmail.com>
2025-06-24 17:03:23 +0530

    Add snipbox-cli formula

---
+++ Formula/snipbox-cli.rb
@@ -0,0 +1,18 @@
+class SnipboxCli < Formula
+  desc "A command-line interface for Snipbox"
+  homepage "https://github.com/Aadithya-J/snipbox-cli"
+  url "https://github.com/Aadithya-J/snipbox-cli/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "27890f92ad69d683e29fd573bc7c5ca32295a0d6f5827967e6edf3e3a3eb9e99"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "snipbox", "./cmd"
+    bin.install "snipbox"
+  end
+
+  test do
+    system "#{bin}/snipbox", "--version"
+  end
+end
